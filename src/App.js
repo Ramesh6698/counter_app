0073c0181e1c2a7bf1efacbea6cf01ea45a230e1
Ramesh6698 <rameshsekar6698@gmail.com>
2023-04-25 18:08:01 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
    <h1> Counter App</h1>
    <h2> {value}</h2>
    <button className="btn btn-outline-success" onClick={()=> setvalue(value +1)}>Increment</button>
-   <button className="btn btn-outline-danger" onClick={()=> setvalue(value -1)}>Degrement</button>
+   <button className="btn btn-outline-danger" onClick={()=> {if (value!=0) {setvalue(value -1 )}}}>Degrement</button>
 
    <button className="btn btn-outline-secondary" onClick={()=> setvalue(0)}>Reset</button>
 
